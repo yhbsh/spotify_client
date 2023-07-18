c9b2ab92aca03805cedc8fb273e0b6418b67a259
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 06:09:27 +0100

    add adaptive grid view and animation

--- lib/design/main_content/discover_songs_grid/songs_grid.dart
+++ lib/design/main_content/discover_songs_grid/songs_grid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 import '../../../entities/song.dart';
 import '../../../notifiers/songs_notifier.dart';
@@ -14,12 +15,29 @@ class SongsGrid extends StatefulWidget {
 class _SongsGridState extends State<SongsGrid> {
   late final SongsNotifier _notifier;
   late final List<Song> _songs;
+  late int _crossAxisCount = 6;
 
   @override
   void initState() {
     _notifier = SongsNotifier();
     _songs = _notifier.songs;
     super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    final size = MediaQuery.sizeOf(context);
+    final width = size.width;
+
+    _crossAxisCount = switch (width) {
+      final w when w < 600 => 2,
+      final w when w < 900 => 3,
+      final w when w < 1200 => 4,
+      final w when w < 1500 => 5,
+      _ => 6,
+    };
+
+    super.didChangeDependencies();
   }
 
   @override
@@ -34,14 +52,20 @@ class _SongsGridState extends State<SongsGrid> {
       child: GridView.builder(
         physics: const ClampingScrollPhysics(),
         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 6,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: _crossAxisCount,
           childAspectRatio: 1 / 1.5,
           mainAxisSpacing: 20,
           crossAxisSpacing: 20,
         ),
         itemCount: _songs.length,
-        itemBuilder: (_, i) => SongGridCard(song: _songs[i]),
+        itemBuilder: (_, i) => AnimationConfiguration.staggeredGrid(
+          position: i,
+          columnCount: _crossAxisCount,
+          child: ScaleAnimation(
+            child: FadeInAnimation(child: SongGridCard(song: _songs[i])),
+          ),
+        ),
       ),
     );
   }
