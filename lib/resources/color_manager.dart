a60f3ee2e9fc9a89dbcb0a43c0bc7922ec4dc258
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 02:49:04 +0100

    Add ColorManager class to manage our app colors and design system.

---
+++ lib/resources/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  const ColorManager._();
+
+  static const primaryColor = Colors.greenAccent;
+  static const sideBarBackgroundColor = Color(0xFF121212);
+}
