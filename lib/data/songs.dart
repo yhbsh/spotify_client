a31e2074cb3a2b970cfa83e69a393e8e540a4b02
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 03:21:59 +0100

    add a small probability that a song does not have an image

--- lib/data/songs.dart
+++ lib/data/songs.dart
@@ -1,14 +1,19 @@
+import 'dart:math' as math;
+
 import 'package:spotify/entities/song.dart';
 
 abstract class SongsData {
   static final songs = List.generate(
-    20,
-    (i) => Song(
-      id: i,
-      name: 'Lorem Ipsum dolor sit amet $i',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-          'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      image: 'https://picsum.photos/200/300?random=$i',
-    ),
+    700,
+    (i) {
+      final randomIndex = math.Random(i).nextInt(700);
+      return Song(
+        id: i,
+        name: 'Lorem Ipsum dolor sit amet $i',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+            'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: i == randomIndex ? null : 'https://picsum.photos/200/300?random=$i',
+      );
+    },
   );
 }
