df65d1d91e2af778f25b1158ac3b6ab6d05b650e
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 02:25:03 +0100

    Adjust window size using desktop_window package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 
 import 'core/spotify.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await DesktopWindow.setWindowSize(const Size(1280, 720));
+  await DesktopWindow.setMaxWindowSize(const Size(1280, 720));
+  await DesktopWindow.setMinWindowSize(const Size(640, 360));
+
   runApp(const Spotify());
 }
