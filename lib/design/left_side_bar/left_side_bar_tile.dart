d768836bc1d4395b9d65ff424ad5b72ba6d8efc9
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 03:34:31 +0100

    finish left side bar design

---
+++ lib/design/left_side_bar/left_side_bar_tile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class LeftSideBarTile extends StatelessWidget {
+  const LeftSideBarTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.onTap,
+  });
+
+  final Widget icon;
+  final String title;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        hoverColor: Colors.white.withOpacity(.1),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          child: Row(
+            children: [
+              icon,
+              const SizedBox(width: 20),
+              Expanded(
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/design/left_side_bar/left_side_bar_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../resources/color_manager.dart';
+import 'left_side_bar_tile.dart';
+
+class LeftSideBarWidget extends StatelessWidget {
+  const LeftSideBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
+    return SizedBox(
+      height: size.height * .9,
+      width: size.width * .15,
+      child: ColoredBox(
+        color: ColorManager.sideBarBackgroundColor,
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Image(
+                image: AssetImage('assets/spotify_logo.png'),
+                fit: BoxFit.cover,
+                color: Colors.white,
+                filterQuality: FilterQuality.high,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Column(
+                children: [
+                  LeftSideBarTile(
+                    onTap: () {},
+                    title: 'Home',
+                    icon: const Icon(Icons.home, color: Colors.white),
+                  ),
+                  LeftSideBarTile(
+                    onTap: () {},
+                    title: 'Search',
+                    icon: const Icon(Icons.search, color: Colors.white),
+                  ),
+                  LeftSideBarTile(
+                    onTap: () {},
+                    title: 'Your Library',
+                    icon: const Icon(Icons.library_music, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
