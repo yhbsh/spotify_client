ddde626ce3ce59e96ddcd7b51870d52725c78f7a
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 03:43:48 +0100

    Add the current song notifier as a change notifier provider

--- lib/core/spotify.dart
+++ lib/core/spotify.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../design/main_screen.dart';
+import '../notifiers/current_song_notifier.dart';
 
 class Spotify extends StatelessWidget {
   const Spotify({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.from(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.greenAccent,
+    return ChangeNotifierProvider.value(
+      value: CurrentSongNotifier(),
+      child: MaterialApp(
+        theme: ThemeData.from(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.greenAccent,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        home: const MainScreen(),
       ),
-      home: const MainScreen(),
     );
   }
 }
