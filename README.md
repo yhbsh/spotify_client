af44ca97388eccdd6613585623afbc949d9ccb16
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 06:19:51 +0100

    Add assets_audio_player package

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ a pixel perfect spotify clone made with flutter for desktop and web
 
 ## Dependencies
 
-cached_network_image, desktop_window, provider, flutter_staggered_animations
+cached_network_image, desktop_window, provider, flutter_staggered_animations, assets_audio_player
 
 ## Dev Dependencies
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+import assets_audio_player
+import assets_audio_player_web
 import desktop_window
 import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AssetsAudioPlayerPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerPlugin"))
+  AssetsAudioPlayerWebPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerWebPlugin"))
   DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
