3e646db0a6560a8ea1259ae61b52984199a78fb4
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 04:09:17 +0100

    adjust layout

--- lib/design/main_screen.dart
+++ lib/design/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'bottom_bar_widget.dart';
+import 'bottom_bar/bottom_bar_widget.dart';
 import 'left_side_bar/left_side_bar_widget.dart';
 import 'main_content/main_content_widget.dart';
 
@@ -9,16 +9,19 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
+    return const Material(
+      color: Colors.transparent,
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              LeftSideBarWidget(),
-              MainContentWidget(),
-            ],
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                LeftSideBarWidget(),
+                MainContentWidget(),
+              ],
+            ),
           ),
           BottomBarWidget(),
         ],
