fc7ad33832db1ac601ea9e71d2cf0a54d4684e41
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-18 05:02:52 +0100

    fix bug: state not reflecting due to not setting the state

--- lib/notifiers/current_song_notifier.dart
+++ lib/notifiers/current_song_notifier.dart
@@ -4,16 +4,17 @@ import '../entities/song.dart';
 import '../states/current_song_state.dart';
 
 class CurrentSongNotifier extends ChangeNotifier {
-  final CurrentSongState _state = const CurrentSongState();
+  CurrentSongState _state = const CurrentSongState();
+  // create a hard copy of the state
   CurrentSongState get state => _state;
 
   void play(Song song) {
-    _state.copyWith(song: song, status: CurrentSongStatus.playing);
+    _state = _state.copyWith(song: song, status: CurrentSongStatus.playing);
     notifyListeners();
   }
 
   void pause() {
-    _state.copyWith(status: CurrentSongStatus.paused);
+    _state = _state.copyWith(status: CurrentSongStatus.paused);
     notifyListeners();
   }
 }
