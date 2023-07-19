833bcd243681c088f16c56e3143ee835ce618bbc
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-19 15:13:58 +0100

    fix layout

--- lib/presentation/design/bottom_bar/bottom_bar_song_player/bottom_bar_songs_player_widget.dart
+++ lib/presentation/design/bottom_bar/bottom_bar_song_player/bottom_bar_songs_player_widget.dart
@@ -7,36 +7,28 @@ import 'repeat_songs_button.dart';
 import 'shuffle_songs_button.dart';
 import 'song_timeline_widget.dart';
 
-class BottomBarSongsPlayerWidget extends StatefulWidget {
+class BottomBarSongsPlayerWidget extends StatelessWidget {
   const BottomBarSongsPlayerWidget({super.key});
 
-  @override
-  State<BottomBarSongsPlayerWidget> createState() => _BottomBarSongsPlayerWidgetState();
-}
-
-class _BottomBarSongsPlayerWidgetState extends State<BottomBarSongsPlayerWidget> {
   @override
   Widget build(BuildContext context) {
     return const Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         SizedBox(height: 4),
-        Expanded(
-          flex: 3,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ShuffleMusicButton(),
-              SizedBox(width: 10),
-              PlayPreviousSongButton(),
-              SizedBox(width: 10),
-              PlayPauseSongButton(),
-              SizedBox(width: 10),
-              PlayNextSongButton(),
-              SizedBox(width: 10),
-              RepeatSongsButton(),
-            ],
-          ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ShuffleMusicButton(),
+            SizedBox(width: 10),
+            PlayPreviousSongButton(),
+            SizedBox(width: 10),
+            PlayPauseSongButton(),
+            SizedBox(width: 10),
+            PlayNextSongButton(),
+            SizedBox(width: 10),
+            RepeatSongsButton(),
+          ],
         ),
         SizedBox(height: 4),
         SongTimelineWidget(),
