00cb3e9748aa6049447a9246aacfe422d89ef404
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-19 15:13:38 +0100

    Add fetch songs and watch songs usecases

--- lib/domain/usecases/songs/fetch_songs.dart
+++ lib/domain/usecases/songs/fetch_songs.dart
@@ -1,26 +1,52 @@
 import 'package:dartz/dartz.dart';
+import 'package:spotify/data/dtos/requests/fetch_songs_request.dart';
 
 import '../../../core/interfaces/failure.dart';
 import '../../../core/interfaces/usecase.dart';
 import '../../entities/song.dart';
+import '../../repositories/songs_repository.dart';
 
 class FetchSongsUseCase implements FutureUseCase<FetchSongsResult, FetchSongsParams> {
-  // final SongsRepository _repository;
+  final SongsRepository _repository;
 
-  // const FetchSongsUseCase(this._repository);
+  const FetchSongsUseCase(this._repository);
 
   @override
-  Future<Either<Failure, FetchSongsResult>> call({required FetchSongsParams params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, FetchSongsResult>> call({required FetchSongsParams params}) async {
+    final request = FetchSongsRequest(page: params.page, limit: params.limit);
+
+    final result = await _repository.fetchSongs(params: request);
+
+    return result.fold(
+      (failure) => Left(failure),
+      (response) => Right(
+        FetchSongsResult(
+          songs: List<Song>.generate(
+            response.songs.length,
+            (i) => Song(
+              id: response.songs[i].id,
+              name: response.songs[i].name,
+              url: response.songs[i].url,
+              image: response.songs[i].image,
+              description: response.songs[i].description,
+              artist: response.songs[i].artist,
+            ),
+          ),
+          page: params.page,
+        ),
+      ),
+    );
   }
 }
 
 class FetchSongsParams {
-  final String query;
   final int page;
+  final int limit;
 
-  const FetchSongsParams({required this.query, required this.page});
+  const FetchSongsParams({
+    required this.page,
+    required this.limit,
+  });
 }
 
 class FetchSongsResult {
--- lib/domain/usecases/songs/watch_songs.dart
+++ lib/domain/usecases/songs/watch_songs.dart
@@ -1,14 +1,51 @@
 import 'package:dartz/dartz.dart';
+import 'package:spotify/data/dtos/requests/watch_songs_request.dart';
 
 import '../../../core/interfaces/failure.dart';
 import '../../../core/interfaces/usecase.dart';
 import '../../entities/song.dart';
+import '../../repositories/songs_repository.dart';
 
 class WatchSongsUseCase implements StreamUseCase<WatchSongsResult, WatchSongsParams> {
+  final SongsRepository _repository;
+
+  const WatchSongsUseCase(this._repository);
+
   @override
   Stream<Either<Failure, WatchSongsResult>> call({required WatchSongsParams params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+    final request = WatchSongsRequest(
+      page: params.page,
+      limit: params.limit,
+      search: params.search,
+      sort: params.sort,
+      order: params.order,
+    );
+
+    final stream = _repository.watchSongs(params: request);
+
+    return stream.map(
+      (result) => result.fold(
+        (failure) => Left(failure),
+        (response) => Right(
+          WatchSongsResult(
+            songs: List<Song>.generate(
+              response.songs.length,
+              (i) => Song(
+                id: response.songs[i].id,
+                name: response.songs[i].name,
+                url: response.songs[i].url,
+                image: response.songs[i].image,
+                description: response.songs[i].description,
+                artist: response.songs[i].artist,
+              ),
+            ),
+            page: response.page,
+            limit: response.limit,
+            total: response.total,
+          ),
+        ),
+      ),
+    );
   }
 }
 
