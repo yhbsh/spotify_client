130f70979a3f5728bd58b2a6c4a6b7ba98856c34
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-19 15:31:35 +0100

    fix name

--- lib/presentation/design/bottom_bar/bottom_bar_widget.dart
+++ lib/presentation/design/bottom_bar/bottom_bar_widget.dart
@@ -20,7 +20,7 @@ class BottomBarWidget extends StatelessWidget {
           children: [
             BottomBarPlayingSongWidget(),
             BottomBarSongsPlayerWidget(),
-            BottomBarSongControllButtons(),
+            BottomVBarVolumeConrolButtons(),
           ],
         ),
       ),
