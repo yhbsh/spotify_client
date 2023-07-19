75280bc740a70b95b69d3decb22b025b9765aee3
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-19 07:10:00 +0100

    Rename discover_songs_grid folder to just songs_grid

--- lib/presentation/design/main_content/main_content_widget.dart
+++ lib/presentation/design/main_content/main_content_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../core/resources/color_manager.dart';
 import 'discover_header.dart';
-import 'discover_songs_grid/songs_grid.dart';
+import 'songs_grid/songs_grid.dart';
 
 class MainContentWidget extends StatelessWidget {
   const MainContentWidget({super.key});
