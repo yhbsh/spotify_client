73c3a9f8fa8b6a1e1845aca78691299e89201cbc
Youcef Houssem Eddine Bousmaha <houssembousmaha.ksu@gmail.com>
2023-07-19 15:12:29 +0100

    add providers

--- lib/core/spotify.dart
+++ lib/core/spotify.dart
@@ -1,6 +1,12 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:spotify/presentation/notifiers/songs_notifier.dart';
 
+import '../data/repositories/audio_player_repository_impl.dart';
+import '../data/repositories/songs_repository_impl.dart';
+import '../domain/repositories/audio_player_repository.dart';
+import '../domain/repositories/songs_repository.dart';
 import '../presentation/design/main_screen.dart';
 import '../presentation/notifiers/current_song_notifier.dart';
 
@@ -9,8 +15,25 @@ class Spotify extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CurrentSongNotifier(context),
+    return MultiProvider(
+      providers: [
+        Provider<SongsRepository>(
+          create: (context) => const SongsRepositoryImpl(),
+          lazy: true,
+        ),
+        Provider<AudioPlayerRepository>(
+          create: (context) => AudioPlayerRepositoryImpl(AssetsAudioPlayer()),
+          lazy: true,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CurrentSongNotifier(context),
+          lazy: true,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => SongsNotifier(context),
+          lazy: true,
+        ),
+      ],
       child: MaterialApp(
         theme: ThemeData.from(
           colorScheme: ColorScheme.fromSeed(
